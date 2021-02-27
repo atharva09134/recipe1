e8d1f4132739d62a757e787bf3fa00a2380bd8ba
atharva vadnere <75987472+atharva09134@users.noreply.github.com>
2021-02-27 21:13:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+                                       VEG BURGER
+									  
+									 
+									 
+									  Ingrendients
+						
+1. potato
+2. burger bun
+3. chees slices
+4. mayonise
+5. tomato suace
+6. onion
+7. green chili
+8. chat masala
+9. grarlic & gnijer
+
+
+                                     process
+
+take one burger bun and chut it from middel and put suace & mayonise put cutlet,chees,onion , etc. 
+
+
+
+                         🍔🍔 and enjoy your VEG BURGER 🍔🍔
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
